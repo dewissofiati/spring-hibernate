eada4590cb5492e672853fc6d31d2579900f33a0
Dewi Syarah Sofiati <42134623+dewissofiati@users.noreply.github.com>
2018-12-04 19:53:07 +0700

    delete constructor and unused import & rename column

--- spring-hibernate-quis/src/main/java/com/tabeldata/springhibernatequis/entity/BukuTabungan.java
+++ spring-hibernate-quis/src/main/java/com/tabeldata/springhibernatequis/entity/BukuTabungan.java
@@ -8,18 +8,15 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "bukutabungan")
+@Table(name = "buku_tabungan")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BukuTabungan {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "kode_transaksi")
     private String kode;
 
@@ -34,7 +31,4 @@ public class BukuTabungan {
 
     @Column(name = "saldo")
     private BigDecimal saldo;
-
-    public BukuTabungan(String kode, LocalDate tanggalTransaksi, BigDecimal debet, BigDecimal kredit, BigDecimal saldo) {
-    }
 }
